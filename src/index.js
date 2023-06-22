a3a83d90bdd334861993771d900c5b60d9dabc8c
iKalashnikova <ikalashnikova@ukr.net>
2023-06-22 17:26:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter 
-      // basename="/goit-react-hw-08-phonebook"
+      basename="/goit-react-hw-08-phonebook"
       >
         <App />
       </BrowserRouter>
