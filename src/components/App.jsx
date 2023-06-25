fc84be88ef13700fd98dffaae35169983fc17196
iKalashnikova <ikalashnikova@ukr.net>
2023-06-25 14:23:50 +0200

    ?

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,15 +4,18 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './pages/login';
 import Layout from './Layout/Layout';
 import Registration from './pages/registration';
-import Home from './pages/home'
+import Home from './pages/home';
+import { useSelector } from 'react-redux';
 
 const App = () => {
+  const profile = useSelector((state) => state.auth.profile);
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element ={<Home/>}/>
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Registration" element={<Registration />} />
+        <Route path="/Login" element={profile ?<PhoneBooks/>:<Login />} />
+        <Route path="/Registration" element={profile ?<PhoneBooks/>: <Registration />} />
         <Route path="/contacts" element={<PhoneBooks />} />
       </Route>
     </Routes>
